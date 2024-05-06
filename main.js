bfbb1236e5ca8193ed410ab145bf83ad47e8cfbb
Dhriti-Malhotra <134493853+Dhriti-Malhotra@users.noreply.github.com>
2024-05-06 17:55:08 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -1,22 +1,57 @@
-function preload() {
+img = "";
+noseX = 0;
+noseY = 0;
+marioX = 325;
+marioY = 325;
+
+function preload()
+{
 	world_start = loadSound("world_start.wav");
 	setSprites();
 	MarioAnimation();
 }
 
-function setup() {
-	canvas = createCanvas(1240,336);
+function setup()
+{
+	canvas = createCanvas(650, 400);
+	video = createCapture(VIDEO);
 	canvas.parent('canvas');
 	
 	instializeInSetup(mario);
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
 }
 
-function draw() {
+function modelLoaded()
+{
+	console.log("Model Loaded!")
+}
+
+function draw()
+{
 	game()
+	if(noseX < 300)
+	{
+		marioX = marioX - 1;
+	}
+	if(noseX > 300)
+	{
+		marioX = marioX + 1;
+	}
+	if(noseY < 150);
+	{
+		marioY = marioY - 1;
+	}
 }
 
-
-
-
+function gotPoses(results)
+{	
+	if(results.length > 0)
+	{
+		noseX = results[0].pose.nose.x;
+		noseY = results[0].pose.nose.y;
+		console.log("noseX = " + noseX + "noseY = " + noseY);
+	}
+}
 
 
